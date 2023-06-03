ef87b24949090cad7b1eae89ae231cb8c241fc89
Jonny Ingall <jonny@mondago.com>
2023-06-03 22:04:06 +0100

    Add validation logic, fix some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-fn guess(target_word: String) -> bool {
+fn guess(target_word: &String) -> bool {
     let mut input = String::new();
     match std::io::stdin().read_line(&mut input) {
         Ok(_) => {
+            if input.trim().len() != 5 {
+                println!("Word must be 5 letters long");
+                guess(&target_word);
+            }
             return input.trim() == target_word;
         }
         Err(error) => panic!("error: {error}"),
@@ -12,10 +16,19 @@ fn main() {
     println!("Hello, world!");
     let word = "happy";
     let max_guesses = 5;
-    for guesses in 1..=max_guesses {
-        if guess(word.to_string()) {
-            println!("Got it in {}! Nice one.", guesses);
+    let mut won = false;
+    for i in 1..=max_guesses {
+        if guess(&word.to_string()) {
+            won = true;
             break;
+        } else {
+            println!("Not quite... ({} guesses remaining)", max_guesses - i);
         }
+    }
+
+    if won {
+        println!("You got it! Nice one.");
+    } else {
+        println!("The word was '{}'", word);
     }
 }
